d0d201b52fb99a0a62d10731c49d21114b3f0c42
Fabian Valencia <fabian.valencia@kushkipagos.com>
2022-04-04 12:28:21 -0500

    Implementing routes to show components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import TimeZone from './components/TimeZone'
 
 const App = () => {
   return (
     <div>
-      <Sidebar></Sidebar>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/home/*' element={<Home />}>
+            <Route path='time-zone' element={<TimeZone />} />
+          </Route>
+          <Route path='/sidebar' element={<Sidebar />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
